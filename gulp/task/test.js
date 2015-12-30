aed8f841654e2cf14be37c057c22eaf85fa75996
zchq88 <zchq88@aliyun.com>
2015-12-30 13:55:26 +0800

    GULP配置修改，test任务打印配置信息

--- gulp/task/test.js
+++ gulp/task/test.js
@@ -2,8 +2,10 @@
  * @file gulp任务test
  * @author zchq88
  */
-var gulp = require('gulp');
+
 var gutil = require('gulp-util');
+var config = require('../config');// gulp公共配置
+
 exports.task = function () {
-    gutil.log(gutil.colors.red('test任务'));
+    gutil.log(gutil.colors.red(config.banner));
 };
