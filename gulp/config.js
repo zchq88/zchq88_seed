00611ef46bb990ab8700d61867aae514aa268275
zchq88 <zchq88@aliyun.com>
2015-12-30 13:37:57 +0800

    增加GULP配置文件

---
+++ gulp/config.js
@@ -0,0 +1,13 @@
+/**
+ * @file gulp配置
+ * @author zchq88
+ */
+var VERSION = require('../../zchq88_web/package.json').version;
+
+module.exports = {
+    banner: '/*!\n'
+    + ' * zchq88_web\n'
+    + ' * @license MIT\n'
+    + ' * v' + VERSION + '\n'
+    + ' */\n'
+};
